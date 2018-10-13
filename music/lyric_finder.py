5414928aa57d9d6a5cc51935e5ac5f1f361c69a0
Angelos Perivolaropoulos <angelos.perivolaropoulos@skyscanner.net>
2018-10-13 09:56:22 +0100

    Added lyrics finder

---
+++ music/lyric_finder.py
@@ -0,0 +1,16 @@
+import lyricwikia
+
+class LyricFinder:
+
+    def __init__(self, lyric_length):
+        self.lyric_length = lyric_length
+    
+    def find_lyrics(self, artist, title, progress):
+        try: 
+            lyrics = lyricwikia.get_lyrics(artist, title)
+            lyrics = lyrics.split()
+            index = int(progress*len(lyrics))
+            sing_along = ' '.join(lyrics[index:(index+self.lyric_length)])
+            return sing_along
+        except lyricwikia.LyricsNotFound:
+            print('Lyrics not found')
